ed48803b371fae72391fc11c68f556900e055c04
lucas-amberg <102396588+lucas-amberg@users.noreply.github.com>
2023-09-30 16:11:25 -0700

    Add date code

--- src/components/DateHeading.tsx
+++ src/components/DateHeading.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+//import React from 'react'
 
 type DateProps = {date: Date}
 
+const months: string[] = ["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"];
+const weekdays: string[] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
 const DateHeading = ({date}:DateProps) => {
   return (
-    <h1>Hello! It is {date.getDay()}, {date.getMonth()} {date.getDate()}. </h1>
+    <h1>Hello! It is {weekdays[date.getDay()]}, {months[date.getMonth()]} {date.getDate()}, {date.getFullYear()}. </h1>
   )
 }
 
